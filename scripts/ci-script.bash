651a650fa5a15e613930985b0a70d51573f5063d
Miguel Ribeiro <miguelribeiro.ap@gmail.com>
2020-04-17 20:00:07 +0100

    one more attempt

--- scripts/ci-script.bash
+++ scripts/ci-script.bash
@@ -1,36 +1,4 @@
 #!/bin/sh
 
-
 echo 'starting script'
-
-# DECIPHER DEVELOPMENT CERTIFICATES
-
-openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in scripts/certs/development-cert.cer.enc -d -a -out scripts/certs/development-cert.cer
-openssl aes-256-cbc -k "$SECURITY_PASSWORD" -in scripts/certs/development-key.p12.enc -d -a -out scripts/certs/development-key.p12
-
-# Create custom keychain
-
-# Create custom keychain
-# security create-keychain -p $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain
-
-# Make the ios-build.keychain default, so xcodebuild will use it
-# security default-keychain -s ios-build.keychain
-
-# Unlock the keychain
-# security unlock-keychain -p $CUSTOM_KEYCHAIN_PASSWORD ios-build.keychain
-
-# Set keychain timeout to 1 hour for long builds
-security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
-
-#security import ./scripts/certs/AppleWWDRCA.cer -k ios-build.keychain -A
-#security import ./scripts/certs/enc-development-cert.cer -k ios-build.keychain -P $SECURITY_PASSWORD -A
-#security import ./scripts/certs/enc-development-key.p12 -k ios-build.keychain -P $SECURITY_PASSWORD -A
-
-# Fix for OS X Sierra that hungs in the codesign step
-#security set-key-partition-list -S apple-tool:,apple: -s -k $SECURITY_PASSWORD ios-build.keychain > /dev/null
-
-#xcodebuild archive -project Timetracker.xcodeproj -configuration Release -archivePath xcbuild/timetracker.xcarchive -scheme Timetracker
-
-#xcodebuild -exportArchive -archivePath xcbuild/timetracker.xcarchive -exportPath xcbuild/output -project Timetracker.xcodeproj -configuration Release -exportOptionsPlist exportOptions.plist 
-
-
+echo 0
