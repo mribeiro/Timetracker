2630affc1403e028d50a77e046289eb5d8f1f901
Antonio Ribeiro <miguelribeiro.ap@gmail.com>
2020-04-24 08:43:02 +0100

    Subtle UI tweaks

--- Timetracker/AppDelegate.swift
+++ Timetracker/AppDelegate.swift
@@ -123,6 +123,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
 
     @IBAction func openPreferences(_ sender: NSMenuItem) {
         let prefId = PreferencePaneIdentifier(sender.identifier?.rawValue ?? "")
+        preferencesWindowController.window?.titlebarAppearsTransparent = true
         preferencesWindowController.show(preferencePane: prefId)
     }
 
@@ -269,73 +270,5 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         managedObjectContext.persistentStoreCoordinator = coordinator
         return managedObjectContext
     }()
-
-    // MARK: - Core Data Saving and Undo support
-
-    @IBAction func saveAction(_ sender: AnyObject!) {
-        // Performs the save action for the application, which is to send the save:
-        //      message to the application's managed object context.
-        // Any encountered errors are presented to the user.
-        if !managedObjectContext.commitEditing() {
-            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing before saving")
-        }
-        if managedObjectContext.hasChanges {
-            do {
-                try managedObjectContext.save()
-            } catch {
-                let nserror = error as NSError
-                NSApplication.shared.presentError(nserror)
-            }
-        }
-    }
-
-    func windowWillReturnUndoManager(_ window: NSWindow) -> UndoManager? {
-        // Returns the NSUndoManager for the application.
-        // In this case, the manager returned is that of the managed object context for the application.
-        return managedObjectContext.undoManager
-    }
-
-    func applicationShouldTerminate(_ sender: NSApplication) -> NSApplication.TerminateReply {
-        // Save changes in the application's managed object context before the application terminates.
-        if !managedObjectContext.commitEditing() {
-            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing to terminate")
-            return .terminateCancel
-        }
-
-        if !managedObjectContext.hasChanges {
-            return .terminateNow
-        }
-
-        do {
-            try managedObjectContext.save()
-        } catch {
-            let nserror = error as NSError
-            // Customize this code block to include application-specific recovery steps.
-            let result = sender.presentError(nserror)
-            if result {
-                return .terminateCancel
-            }
-
-            let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?",
-                                             comment: "Quit without saves error question message")
-            let info =
-                NSLocalizedString("Quitting now will lose any changes you have made since the last successful save",
-                                  comment: "Quit without saves error question info")
-            let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
-            let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
-            let alert = NSAlert()
-            alert.messageText = question
-            alert.informativeText = info
-            alert.addButton(withTitle: quitButton)
-            alert.addButton(withTitle: cancelButton)
-
-            let answer = alert.runModal()
-            if answer == NSApplication.ModalResponse.alertFirstButtonReturn {
-                return .terminateCancel
-            }
-        }
-        // If we got here, it is time to quit.
-        return .terminateNow
-    }
 
 }
